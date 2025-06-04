77ccc30525bc6ae6dd2ba0a0dcf365692c7353ca
Jeff Eilbott <jeilbott@surveybott.com>
2025-06-04 08:45:44 -0400

    change confound spec, drop volumes

--- fmriprep_denoise.m
+++ fmriprep_denoise.m
@@ -5,23 +5,50 @@
 function [nifti,cmd] = fmriprep_denoise(in,varargin)
 p = inputParser;
 p.addRequired('in',@isfolder);
-p.addParameter('out',pwd,@ischar);
 p.addParameter('ses',false);
+p.addParameter('out',pwd,@ischar);
 p.addParameter('write',true,@(x) islogical(x) || isnumeric(x));
-p.addParameter('overwrite',true,@(x) islogical(x) || isnumeric(x));
 p.addParameter('desc','denoised',@ischar);
+
+p.addParameter('confounds',{'rmsd','global_signal'},@iscellstr);
+p.addParameter('motionParams',24,@(x) ismember(x,[0 6 12 18 24]));
+p.addParameter('acompcor',10);
+p.addParameter('aroma',true); 
+p.addParameter('bandpass',[],@(x) isnumeric(x) && numel(x)==2);
+p.addParameter('tr',[],@isscalar);
+p.addParameter('polort',2,@isscalar);
+p.addParameter('dropVols',0,@isscalar);
+
 p.addParameter('funcStr','*_desc-smoothAROMAnonaggr_bold.nii.gz',@ischar);
 p.addParameter('confoundStr','_desc-confounds_timeseries.tsv',@ischar);
-p.addParameter('confounds',{'framewise_displacement','trans_x','trans_y','trans_z','rot_x','rot_y','rot_z',...
-    'a_comp_cor_00','a_comp_cor_01','a_comp_cor_02','a_comp_cor_03','a_comp_cor_04','a_comp_cor_05',...
-    'global_signal'},@iscellstr);
-p.addParameter('bandpass',[],@(x) isnumeric(x) && numel(x)==2);
-p.addParameter('tr',[],@(x) isnumeric(x) && numel(x) == 1);
-p.addParameter('polort',2,@(x) isnumeric(x) && numel(x)==1);
 p.addParameter('includeSub',[],@isnumeric);
-p.addParameter('includeTask',{},@(x) ischar(x) || iscellstr(x));
+p.addParameter('includeTask',{},@(x) ischar(x) || iscellstr(x) || isstring(x));
+
+p.addParameter('overwrite',true,@(x) islogical(x) || isnumeric(x));
 p.parse(in,varargin{:});
 inputs = p.Results;
+
+% add motion parameters
+if inputs.motionParams
+    base = {'trans_x','trans_y','trans_z','rot_x','rot_y','rot_z'};
+    mopar = base;
+    if inputs.motionParams == 12
+        mopar = [mopar cellfun(@(x) [x '_derivative1'],base,'UniformOutput',0)];
+    end
+    if inputs.motionParams == 18
+        mopar = [mopar cellfun(@(x) [x '_power2'],base,'UniformOutput',0)];
+    end
+    if inputs.motionParams == 24
+        mopar = [mopar cellfun(@(x) [x '_derivative1_power2'],base,'UniformOutput',0)];
+    end
+    inputs.confounds = [inputs.confounds mopar];
+end
+% add a_comp_cor
+if inputs.acompcor
+    for i=0:inputs.acompcor-1
+        inputs.confounds{end+1} = sprintf('a_comp_cor_%02d',i);
+    end
+end
 % make out folder absolute
 if ~strcmp(inputs.out(1),filesep)
    inputs.out = fullfile(pwd,inputs.out);
@@ -70,8 +97,22 @@ else
                 t = readtable(tsv,'FileType','text','Delimiter','\t','TreatAsEmpty','n/a');
                 if isempty(inputs.confounds) || all(ismember(inputs.confounds,t.Properties.VariableNames))
                     nifti(i).confound_all = t;
-                    nifti(i).confound = t(:,ismember(t.Properties.VariableNames,inputs.confounds));
+                    var = t.Properties.VariableNames;
+                    % named confounds
+                    colIdx = ismember(var,inputs.confounds);
+                    if sum(colIdx) < numel(inputs.confounds)
+                        warning('%d confounds not found in %s',colIdx,tsv);
+                    end
+                    % aroma
+                    if inputs.aroma
+                        colIdx = colIdx | startsWith(var,'aroma_');
+                    end
+                    nifti(i).confound = t(:,colIdx);
                     nifti(i).confound = fillmissing(nifti(i).confound,'constant',0);
+                    % drop initial volumes
+                    if inputs.dropVols
+                        nifti(i).confounds = nifti(i).confound(inputs.dropVols+1,:);
+                    end
                 end
             end
         end
@@ -125,9 +166,18 @@ else
         else
            bandpass = ''; 
         end
+        % drop initial volumes
+        if inputs.dropVols
+            in = fullfile(inputs.out,regexprep(nifti(i).name,['_' nifti(i).suffix],sprintf('_drop%d.nii.gz', inputs.dropVols)));
+            precmd = sprintf('fslmaths %s %s %d -1; ', nifti(i).file, in, inputs.dropVols);
+        else
+            precmd = '';
+            in = nifti(i).file;
+        end
         % cmd
         cmd{i} = sprintf('3dTproject -input ''%s'' -prefix ''%s'' -polort %d%s%s',...
-            nifti(i).file,out,inputs.polort,ort,bandpass);
+            in,out,inputs.polort,ort,bandpass);
+        cmd{i} = [precmd cmd{i}];
     end
     % write cmd file
     if inputs.write
